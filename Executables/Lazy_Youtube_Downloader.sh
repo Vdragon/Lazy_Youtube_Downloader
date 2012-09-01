bd33bf1ce1f4de09653ddbd4cbaae81a8a61140a
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-09-02 01:35:36 +0800

    加入所有檔案

---
+++ Executables/Lazy_Youtube_Downloader.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# 上一句用來宣告執行script用的shell
+####################################################
+# Script程式名稱 | Script Name
+# 	懶人Youtube影片下載器
+# 	Lazy Youtube Downloader
+# 著作權宣告 | Copyright Declaration
+# 	Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012
+# 授權條款宣告 | License Declaration
+# 	GNU Lesser General Public License version 3.0 or later
+# 	http://www.gnu.org/licenses/lgpl.html
+# 傳回值定義 | Return Value Definition
+# 	0
+# 		正常結束
+# 已知問題 | Known Issues
+# 	Known issues is now on GitHub
+# 	
+# 修訂紀錄 | Changelog
+# 	Changelog is now on GitHub
+# 	
+###################################################
+echo "\
+懶人Youtube影片下載器
+Lazy Youtube Downloader
+Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012"
+
+read -p "\
+請輸入影片所在的完整網址(URL)：
+Please enter the complete URL of the video:
+（等待使用者輸入 | Wait for user input）" video_url
+
+echo "\
+下列命令已被執行：
+Following command is executed:
+youtube-dl --output='%(title)s.%(ext)s' --console-title --continue \"${video_url}\""
+
+youtube-dl --output='%(title)s.%(ext)s' --console-title --continue "${video_url}"
+
+echo "\
+下載程序已執行完畢
+Download process completed"
+
+# 正常結束script程式
+exit 0
---
+++ Installer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# 上一句用來宣告執行script用的shell
+####################################################
+# Script程式名稱 | Script Name
+# 	懶人Youtube影片下載器 安裝程式
+# 	Lazy Youtube Downloader installer
+# 著作權宣告 | Copyright Declaration
+# 	Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012
+# 授權條款宣告 | License Declaration
+# 	GNU Lesser General Public License version 3.0 or later
+# 	http://www.gnu.org/licenses/lgpl.html
+# 傳回值定義 | Return Value Definition
+# 	0
+# 		正常結束
+# 已知問題 | Known Issues
+# 	Known issues is now on GitHub
+# 	
+# 修訂紀錄 | Changelog
+# 	Changelog is now on GitHub
+# 
+# 注意 | Attention
+# 	本程式可能需要系統管理權限才能正常運作
+# 	This program may need system administrative previleges to work normally
+###################################################
+mkdir --parents /usr/local/src/Lazy_Youtube_Downloader
+cp --recursive . /usr/local/src/Lazy_Youtube_Downloader
+cd /usr/local/src/Lazy_Youtube_Downloader
+ln --symbolic --force "${PWD}/Executables/Lazy_Youtube_Downloader.sh" /usr/local/bin/lazy-youtube-dl
+
+# 正常結束
+exit 0
